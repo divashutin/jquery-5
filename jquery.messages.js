43d18d9f2f803b136d586a9aabba6bca22b16759
filipenyakaterina <katerina.filipenya@tut.by>
2014-07-16 18:29:28 +0300

    message

---
+++ jquery.messages.js
@@ -0,0 +1,24 @@
+﻿// JavaScript source code
+
+$(document).ready(function () {
+                $("a").click(function () {
+                    var HeightDocument = $(document).height();
+                    var WidthDocument = $(document).width();
+                    var HeightScreen = $(window).height();
+                    $(".modal_bg").css({ "width": WidthDocument, "height": HeightDocument });
+                    $(".modal_bg").fadeIn(1000);
+                    $(".modal_bg").fadeTo("slow", 0.8);
+                    var Top_modal_window = $(document).scrollTop() + HeightScreen / 2 - $(".modal_window").height() / 2;
+                    $(".modal_window").css({ "top": Top_modal_window + "px", "display": "block" });
+                    $("body").css({ "overflow": "hidden" });
+                    return false;
+                });
+                $(".modal_window button").click(function () {
+                    $(".modal_bg, .modal_window").hide();
+                    $("body").css({ "overflow": "auto" })
+                });
+                $(".modal_bg").click(function () {
+                    $(".modal_bg, .modal_window").hide();
+                    $("body").css({ "overflow": "auto" });
+                });
+            });
