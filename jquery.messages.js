287dc3cd3776542ab72191896bbcfb4528ba01db
filipenyakaterina <katerina.filipenya@tut.by>
2014-07-16 23:32:20 +0300

    with parameters

--- jquery.messages.js
+++ jquery.messages.js
@@ -1,24 +1,44 @@
 ﻿// JavaScript source code
 
 $(document).ready(function () {
-                $("a").click(function () {
-                    var HeightDocument = $(document).height();
-                    var WidthDocument = $(document).width();
-                    var HeightScreen = $(window).height();
-                    $(".modal_bg").css({ "width": WidthDocument, "height": HeightDocument });
-                    $(".modal_bg").fadeIn(1000);
-                    $(".modal_bg").fadeTo("slow", 0.8);
-                    var Top_modal_window = $(document).scrollTop() + HeightScreen / 2 - $(".modal_window").height() / 2;
-                    $(".modal_window").css({ "top": Top_modal_window + "px", "display": "block" });
-                    $("body").css({ "overflow": "hidden" });
-                    return false;
-                });
-                $(".modal_window button").click(function () {
-                    $(".modal_bg, .modal_window").hide();
-                    $("body").css({ "overflow": "auto" })
-                });
-                $(".modal_bg").click(function () {
-                    $(".modal_bg, .modal_window").hide();
-                    $("body").css({ "overflow": "auto" });
-                });
-            });
+    $.fn.message({ text: "Hello! Click anywhere to close me.",delay:10000 });
+});
+
+(function ($) {
+    $.fn.message = function (options) {
+        var options = $.extend({
+            text: "Hello World!",
+            delay:5000
+        }, options);
+
+        var timeout;
+
+
+        var hideMessage = function () {
+
+            $(".modal_bg, .modal_window").hide();
+            $("body").css({ "overflow": "auto" });
+            clearTimeout(timeout);
+        }
+
+
+        $("a").click(function () {
+            var HeightDocument = $(document).height();
+            var WidthDocument = $(document).width();
+            var HeightScreen = $(window).height();
+            $(".modal_bg").css({ "width": WidthDocument, "height": HeightDocument });
+            $(".modal_bg").fadeIn(1000);
+            $(".modal_bg").fadeTo("slow", 0.8);
+            var Top_modal_window = $(document).scrollTop() + HeightScreen / 2 - $(".modal_window").height() / 2;
+            $(".modal_window").css({ "top": Top_modal_window + "px", "display": "block" });
+            $("label").text(options.text);
+            $("body").css({ "overflow": "hidden" });
+            timeout = setTimeout(hideMessage,options.delay);
+        });
+
+        $(".modal_window button").click(hideMessage);
+
+        $(".modal_bg").click(hideMessage);
+    }
+})(jQuery);
+
